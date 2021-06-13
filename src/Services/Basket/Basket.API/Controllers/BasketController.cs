f8dffb170d6968bfdc9ac58d5f6f58317ed09b84
Francesco Grasso <grassofrancesco89@gmail.com>
2021-06-13 11:48:44 +0200

    Portainer Configuration in docker

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Basket.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class BasketController : ControllerBase
     {
