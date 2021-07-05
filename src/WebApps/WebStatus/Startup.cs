0e1500bb6f307cf6f0efa99e0588eac5270900d3
Francesco Grasso <grassofrancesco89@gmail.com>
2021-07-05 11:41:10 +0200

    WebStatus initial configuration healthcheck UI

--- src/WebApps/WebStatus/Startup.cs
+++ src/WebApps/WebStatus/Startup.cs
@@ -23,6 +23,9 @@ namespace WebStatus
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddHealthChecksUI()
+                    .AddInMemoryStorage();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +47,8 @@ namespace WebStatus
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecksUI();
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
