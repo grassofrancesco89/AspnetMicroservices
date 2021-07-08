068a5ed70a558ab12b5d20a26965f82b0fbd98ef
Francesco Grasso <grassofrancesco89@gmail.com>
2021-07-08 17:13:38 +0200

    - Retry Movie.API project

---
+++ src/Services/Movie/Movie.API/Data/MovieAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies.API.Models;
+
+namespace Movie.API.Data
+{
+    public class MovieAPIContext : DbContext
+    {
+        public MovieAPIContext (DbContextOptions<MovieAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.API.Models.Movie> Movie { get; set; }
+    }
+}
