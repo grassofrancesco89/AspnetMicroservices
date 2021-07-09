b7c034d2f753529df4bd241965d7a8d3a407100e
Francesco Grasso <grassofrancesco89@gmail.com>
2021-07-09 14:20:02 +0200

    Movies.Client ef core non serve più perchè uso i servizi..elimino il context, libreria, packages, connectionstring

--- src/WebApps/Movies.Client/Controllers/MoviesController.cs
+++ src/WebApps/Movies.Client/Controllers/MoviesController.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Movies.Client.ApiServices.Interfaces;
 using Movies.Client.Models;
 
@@ -21,22 +20,22 @@ namespace Movies.Client.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
-        {
-            await LogTokenAndClaims();
-            return View(await _movieApiService.GetMovies());
-        }
-        public async Task LogTokenAndClaims()
-        {
-            var identityToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+        //public async Task<IActionResult> Index()
+        //{
+        //    await LogTokenAndClaims();
+        //    return View(await _movieApiService.GetMovies());
+        //}
+        //public async Task LogTokenAndClaims()
+        //{
+        //    var identityToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
 
-            Debug.WriteLine($"Identity token: {identityToken}");
+        //    Debug.WriteLine($"Identity token: {identityToken}");
 
-            foreach (var claim in User.Claims)
-            {
-                Debug.WriteLine($"Claim type: {claim.Type} - Claim value: {claim.Value}");
-            }
-        }
+        //    foreach (var claim in User.Claims)
+        //    {
+        //        Debug.WriteLine($"Claim type: {claim.Type} - Claim value: {claim.Value}");
+        //    }
+        //}
 
         //[Authorize(Roles = "admin")]
         //public async Task<IActionResult> OnlyAdmin()
--- src/WebApps/Movies.Client/Startup.cs
+++ src/WebApps/Movies.Client/Startup.cs
@@ -1,15 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Movies.Client.Data;
 using Movies.Client.ApiServices.Interfaces;
 using Movies.Client.ApiServices;
 
@@ -31,8 +24,6 @@ namespace Movies.Client
 
             services.AddScoped<IMovieApiService, MovieApiService>();
 
-            services.AddDbContext<MoviesClientContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MoviesClientContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
